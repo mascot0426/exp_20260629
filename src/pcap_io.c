b23c613a47991761ff5a80db4992ccdfc312fcc6
CsevenY <a15907670859@163.com>
2026-07-03 18:28:28 +0800

    feat: 实现PCAP离线回放

--- src/pcap_io.c
+++ src/pcap_io.c
@@ -60,4 +60,59 @@ void pcap_save_close(pcap_save_ctx_t *ctx)
     free(ctx);
 }
 
-/* TODO: 离线读取功能将在下一次提交中实现 */
+/* ===== 离线读取功能 ===== */
+
+pcap_t *pcap_replay_open(const char *filename, char *errbuf)
+{
+    pcap_t *handle = pcap_open_offline(filename, errbuf);
+    if (handle == NULL) {
+        fprintf(stderr, "[pcap_io] 打开离线文件失败: %s\n", errbuf);
+    } else {
+        printf("[pcap_io] 已打开离线文件: %s\n", filename);
+    }
+    return handle;
+}
+
+int pcap_replay_loop(const char *filename, const char *filter_str,
+                     pcap_handler callback, void *user_data)
+{
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    /* 打开离线文件 */
+    pcap_t *handle = pcap_replay_open(filename, errbuf);
+    if (handle == NULL) {
+        return -1;
+    }
+
+    /* 设置过滤器(如果指定) */
+    struct bpf_program fp;
+    int has_filter = 0;
+    if (filter_str != NULL) {
+        bpf_u_int32 netmask = 0;  /* 离线文件用0作为netmask */
+        if (pcap_compile(handle, &fp, filter_str, 1, netmask) == 0) {
+            if (pcap_setfilter(handle, &fp) == 0) {
+                has_filter = 1;
+            } else {
+                fprintf(stderr, "[pcap_io] 设置过滤器失败: %s\n", pcap_geterr(handle));
+            }
+        } else {
+            fprintf(stderr, "[pcap_io] 编译过滤器失败: %s\n", pcap_geterr(handle));
+        }
+    }
+
+    /* 读取数据包 */
+    printf("[pcap_io] 开始回放...\n");
+    int ret = pcap_loop(handle, -1, callback, (u_char *)user_data);
+
+    if (ret == -1) {
+        fprintf(stderr, "[pcap_io] 回放错误: %s\n", pcap_geterr(handle));
+    }
+
+    /* 清理 */
+    if (has_filter) {
+        pcap_freecode(&fp);
+    }
+    pcap_close(handle);
+    printf("[pcap_io] 回放结束\n");
+    return (ret == -1) ? -1 : 0;
+}
